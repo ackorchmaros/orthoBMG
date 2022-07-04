8588414f339bcbdf07773d19c4955b168eda59a1
Annachiara Korchmaros <45043205+ackorchmaros@users.noreply.github.com>
2022-07-04 11:00:58 +0200

    download dataset
    small dataset

---
+++ download_data.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+############################################################################
+############################  INPUT DATA ###################################
+############################################################################
+
+# author: Annachira Korchmaros
+# last update: 01.07.2022, Bioinformatics Leipzig
+
+########## STEPS
+#(1) set up path
+#(2) download data
+###########
+
+######### PARAMETERS
+mypath = "/scr/k70san/annachiara"
+echo $mypath
+#analysis_folder = "orthoBMG"
+#data_folder = "$analysis_folder/data"
+
+#(1)
+#cd $mypath
+#mkdir data_folder
+
+#(2)
+# Acidithiobacillus ferrooxidans ATCC 23270, complete genome (NC_011761)
+#wget https://www.ncbi.nlm.nih.gov/nuccore/NC_011761.1?report=fasta -P $data_folder
+
+
+
+
