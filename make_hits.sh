17d4a71cf34769dd31b944e1d9f1831625e8cac0
ackorchmaros <anna.korchmaros@gmail.com>
2022-07-07 15:01:14 +0200

    final version

--- make_hits.sh
+++ make_hits.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --time=00:45:00
-#SBATCH --partition=main
-#SBATCH --ntasks=5
-#SBATCH --job-name=hits
-#SBATCH --output=hits.%j.out
-
 
 ############################################################################
 ############################  HITS ###################################
 ############################################################################
 
-# author: Annachira Korchmaros
-# last update: 05.07.2022, Bioinformatics Leipzig
-# running : sh /scr/k70san/annachiara/orthoBMG/scripts/make_hits.sh /scr/k70san/annachiara/orthoBMG
+# author: Annachiara Korchmaros
+# last update: 06.07.2022, Bioinformatics Leipzig
 ########## STEPS
 #(1) run proteinortho
 #(2) extract hits
@@ -23,8 +15,11 @@
 #mypath = $1  " /scr/k70san/annachiara/orthoBMG 
 data_folder="$1/data"
 output_folder="$1/output"
+#########
+
+############################### MAIN
 
 #(1)
 mkdir $output_folder
 cd $output_folder
-perl $1/scripts/proteinortho-master/proteinortho6.pl -project=output -p=blastp+ $data_folder/*fna -temp=$1/temp
+perl $1/scripts/proteinortho-master/proteinortho6.pl -project=output $data_folder/*fna -temp=$1/temp
